d0760133f1326859b72b96b97901601cdf4fb43b
Aedrinios <clementclerc2@gmail.com>
2021-07-05 16:02:50 +0200

    class Vector (need testing)

--- src/Tools/Vector.h
+++ src/Tools/Vector.h
@@ -4,7 +4,7 @@
 
 #ifndef RAYTRACING_VECTOR_H
 #define RAYTRACING_VECTOR_H
-
+#include <iostream>
 
 class Vector {
 private:
@@ -13,17 +13,20 @@ private:
 public:
 
     Vector(float x, float y, float z): m_x(x), m_y(y), m_z(z){}
+    Vector(const Vector& v);
 
-    float norm();
-    Vector normalized();
-    float dot(Vector v);
+    float norm() const;
+    Vector normalized() const;
+    float dot(const Vector& v) const;
 
-    float operator[](const int i) const;
-    Vector operator+(Vector v);
-    Vector operator-(Vector v);
-    Vector operator*(int factor);
-    Vector operator/(int divide);
+    float operator[](int i) const;
+    Vector operator+(const Vector& v) const;
+    Vector operator-(Vector& v);
+    Vector operator-() const;
+    Vector operator*(float factor) const;
+    Vector operator/(float divide) const;
 };
 
+std::ostream& operator<<(std::ostream& os, const Vector& v);
 
 #endif //RAYTRACING_VECTOR_H
