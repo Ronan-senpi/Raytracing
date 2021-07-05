2f177e6605dccfd4037dedc385275a2802768573
Ronan <r.dhersignerie@gmail.com>
2021-07-05 17:16:11 +0200

    Object

---
+++ src/Tools/Object.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rdher on 05/07/2021.
+//
+
+#include "Object.h"
---
+++ src/Tools/Object.h
@@ -0,0 +1,16 @@
+//
+// Created by rdher on 05/07/2021.
+//
+
+#ifndef RAYTRACING_OBJECT_H
+#define RAYTRACING_OBJECT_H
+
+#include "Entity.h"
+
+class Object : public Entity {
+public:
+    
+};
+
+
+#endif //RAYTRACING_OBJECT_H
