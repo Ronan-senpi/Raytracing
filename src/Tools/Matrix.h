fd2ec5c647f8610450e2399a7ad704174582deaa
Ronan <r.dhersignerie@gmail.com>
2021-07-05 16:04:04 +0200

    Entity

---
+++ src/Tools/Matrix.h
@@ -0,0 +1,34 @@
+////
+//// Created by rdher on 05/07/2021.
+////
+//
+//#ifndef RAYTRACING_MATRIX_H
+//#define RAYTRACING_MATRIX_H
+//
+//#include <array>
+//#include <vector>
+//
+////Matrix 4x4
+//class Matrix {
+//private:
+//    std::array<float, 4 * 4> m{};
+//
+//    static float invf(int i, int j, const std::array<float, 4 * 4> *mat);
+//
+//    static std::array<float, 4 * 4> emptyMatrix();
+//
+//public:
+//
+//    Matrix();
+//
+//    Matrix(const Matrix &mat);
+//
+//    ~Matrix() = default;
+//
+//    Matrix inverse() const;
+//
+//    float operator()(int i, int j) const;
+//};
+//
+//
+//#endif //RAYTRACING_MATRIX_H
--- src/Tools/Vector.h
+++ src/Tools/Vector.h
@@ -8,21 +8,51 @@
 
 class Vector {
 private:
-    float m_x,m_y,m_z;
+    float m_x, m_y, m_z;
 
 public:
 
-    Vector(float x, float y, float z): m_x(x), m_y(y), m_z(z){}
+    Vector(float x, float y, float z) : m_x(x), m_y(y), m_z(z) {}
 
     float norm();
+
     Vector normalized();
+
     float dot(Vector v);
 
     float operator[](const int i) const;
+
     Vector operator+(Vector v);
+
     Vector operator-(Vector v);
+
     Vector operator*(int factor);
+
     Vector operator/(int divide);
+
+    float X() const {
+        return m_x;
+    }
+
+    void X(float val) {
+        m_x = val;
+    }
+
+    float Y() const {
+        return m_y;
+    }
+
+    void Y(float val) {
+        m_y = val;
+    }
+
+    float Z() const {
+        return m_z;
+    }
+
+    void Z(float val) {
+        m_z = val;
+    }
 };
 
 
