c6e3e1b699f1dc2adbc0dfdd3f6598759b1b51cb
Aedrinios <clementclerc2@gmail.com>
2021-07-05 11:09:12 +0200

    wip vector

---
+++ Vector.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Aedrinios on 05/07/2021.
+//
+
+#include "Vector.h"
+
+float Vector::norm()
+{
+
+}
---
+++ Vector.h
@@ -0,0 +1,29 @@
+//
+// Created by Aedrinios on 05/07/2021.
+//
+
+#ifndef RAYTRACING_VECTOR_H
+#define RAYTRACING_VECTOR_H
+
+
+class Vector {
+private:
+    float m_x,m_y,m_z;
+
+public:
+
+    Vector(float x, float y, float z): m_x(x), m_y(y), m_z(z);
+
+    float norm();
+    Vector normalized();
+    float dot(Vector v);
+
+    float operator[](const int i) const;
+    Vector operator+(Vector v);
+    Vector operator-(Vector v);
+    Vector operator*(int factor);
+    Vector operator/(int divide);
+};
+
+
+#endif //RAYTRACING_VECTOR_H
