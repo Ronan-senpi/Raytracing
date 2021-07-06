dc2ee15271ade0909e7aa6814f6f853d3838a005
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2021-07-06 15:16:34 +0200

    add scene

---
+++ src/Tools/Scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rdher on 06/07/2021.
+//
+
+#include "Scene.h"
---
+++ src/Tools/Scene.h
@@ -0,0 +1,13 @@
+//
+// Created by rdher on 06/07/2021.
+//
+
+#ifndef RAYTRACING_SCENE_H
+#define RAYTRACING_SCENE_H
+
+
+class Scene {
+};
+
+
+#endif //RAYTRACING_SCENE_H
