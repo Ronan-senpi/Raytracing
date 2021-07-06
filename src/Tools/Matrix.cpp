681a6496ba8fd9695b7de3f0199918d206fc4e5e
Aedrinios <clementclerc2@gmail.com>
2021-07-06 19:25:03 +0200

    wip matrix inversion

--- src/Tools/Matrix.cpp
+++ src/Tools/Matrix.cpp
@@ -1,20 +1,25 @@
-////
-//// Created by rdher on 05/07/2021.
-////
 //
-//#include <iostream>
-//#include "Matrix.h"
+// Created by rdher on 05/07/2021.
 //
-////Matrix de 4x4
-//
-//Matrix::Matrix() {
-//    m = Matrix::emptyMatrix();
-//}
-//
-//Matrix::Matrix(const Matrix &mat) {
-//}
-//
-//
+
+#include <iostream>
+#include "Matrix.h"
+
+//Matrix de 4x4
+Matrix::Matrix() {
+    m = Matrix::emptyMatrix();
+}
+
+Matrix::Matrix(const Matrix &mat) {
+    m = mat.m;
+}
+
+Matrix Matrix::inverse() const {
+    Matrix resultInv;
+
+    return resultInv;
+}
+
 //Matrix Matrix::inverse() const {
 //    Matrix out;
 //    Matrix inv;
@@ -39,21 +44,121 @@
 //
 //    return out;
 //}
-//
-//float Matrix::operator()(int i, int j) const {
-//    return m[i * 4 + j];
-//}
-//
-//std::array<float, 4 * 4> Matrix::emptyMatrix() {
-//    std::array<float, 4 * 4> a{};
-//    for (int i = 0; i < 4; ++i) {
-//        for (int j = 0; j < 4; ++j) {
-//            a[i = 4 + j] = 0;
-//        }
-//    }
-//    return a;
-//}
-//
+
+float Matrix::operator()(const int i, const int j) const {
+    return m[i * 4 + j];
+}
+
+float &Matrix::operator()(const int i, const int j) {
+    return m[i * 4 + j];
+}
+
+std::array<float, 16> Matrix::emptyMatrix() {
+    std::array<float, 4 * 4> a{};
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            a[i = 4 + j] = 0;
+        }
+    }
+    return a;
+}
+
+float Matrix::determinant() {
+    return (*this)(0, 0) *
+           (((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+            ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2))
+            - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) - ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) -
+            ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2)))
+           - (*this)(1, 0) *
+             (((*this)(0, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(0, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+              ((*this)(0, 3) * (*this)(2, 1) * (*this)(3, 2))
+              - ((*this)(0, 3) * (*this)(2, 2) * (*this)(3, 1)) - ((*this)(0, 2) * (*this)(2, 1) * (*this)(3, 3)) -
+              ((*this)(0, 1) * (*this)(2, 3) * (*this)(3, 2)))
+           + (*this)(2, 0) *
+             (((*this)(0, 1) * (*this)(1, 2) * (*this)(3, 3)) + ((*this)(0, 2) * (*this)(1, 3) * (*this)(3, 1)) +
+              ((*this)(0, 3) * (*this)(1, 1) * (*this)(3, 2))
+              - ((*this)(0, 3) * (*this)(1, 2) * (*this)(3, 1)) - ((*this)(0, 2) * (*this)(1, 3) * (*this)(3, 3)) -
+              ((*this)(0, 1) * (*this)(1, 3) * (*this)(3, 2)))
+           - (*this)(3, 0) *
+             (((*this)(0, 1) * (*this)(1, 2) * (*this)(2, 3)) + ((*this)(0, 2) * (*this)(1, 3) * (*this)(2, 1)) +
+              ((*this)(0, 3) * (*this)(1, 1) * (*this)(2, 2))
+              - ((*this)(0, 3) * (*this)(1, 2) * (*this)(2, 1)) - ((*this)(0, 2) * (*this)(1, 1) * (*this)(2, 3)) -
+              ((*this)(0, 1) * (*this)(1, 3) * (*this)(2, 2)));
+}
+
+Matrix Matrix::adjugate() {
+    Matrix adj;
+    adj(0, 0) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(0, 1) = -((*this)(0, 1) * (*this)(2, 2) * (*this)(3, 3)) - ((*this)(0, 2) * (*this)(2, 3) * (*this)(3, 1)) -
+                ((*this)(0, 3) * (*this)(2, 1) * (*this)(3, 2)) + ((*this)(0, 3) * (*this)(2, 2) * (*this)(3, 1)) +
+                ((*this)(0, 2) * (*this)(2, 1) * (*this)(3, 3)) + ((*this)(0, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(0, 2) = ((*this)(0, 1) * (*this)(1, 2) * (*this)(3, 3)) + ((*this)(0, 2) * (*this)(1, 3) * (*this)(3, 1)) +
+                ((*this)(0, 3) * (*this)(1, 1) * (*this)(3, 2)) - ((*this)(0, 3) * (*this)(1, 2) * (*this)(3, 1)) -
+                ((*this)(0, 2) * (*this)(1, 1) * (*this)(3, 3)) - ((*this)(0, 1) * (*this)(1, 3) * (*this)(3, 2));
+
+    adj(0, 3) = -((*this)(0, 1) * (*this)(1, 2) * (*this)(2, 3)) - ((*this)(0, 2) * (*this)(1, 3) * (*this)(2, 1)) -
+                ((*this)(0, 3) * (*this)(1, 1) * (*this)(2, 2)) + ((*this)(0, 3) * (*this)(1, 2) * (*this)(2, 1)) +
+                ((*this)(0, 2) * (*this)(1, 1) * (*this)(2, 3)) + ((*this)(0, 1) * (*this)(1, 3) * (*this)(2, 2));
+
+
+    // ICI CONNARD
+    adj(1, 0) = -((*this)(1, 0) * (*this)(2, 2) * (*this)(3, 3)) - ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 0)) -
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) + ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) +
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) + ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(1, 1) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(1, 2) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(1, 3) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(2, 0) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(2, 1) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(2, 2) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(2, 3) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(3, 0) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(3, 1) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(3, 2) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+    adj(3, 3) = ((*this)(1, 1) * (*this)(2, 2) * (*this)(3, 3)) + ((*this)(1, 2) * (*this)(2, 3) * (*this)(3, 1)) +
+                ((*this)(1, 3) * (*this)(2, 1) * (*this)(3, 2)) - ((*this)(1, 3) * (*this)(2, 2) * (*this)(3, 1)) -
+                ((*this)(1, 2) * (*this)(2, 1) * (*this)(3, 3)) - ((*this)(1, 1) * (*this)(2, 3) * (*this)(3, 2));
+
+
+    return adj;
+}
+
+
 //float Matrix::invf(int i, int j, const std::array<float, 4 * 4> *mat) {
 //    int o = 2 + (j - i);
 //
@@ -74,5 +179,5 @@
 //
 //#undef e
 //}
-//
-//
+
+
--- src/Tools/Matrix.h
+++ src/Tools/Matrix.h
@@ -1,34 +1,37 @@
-////
-//// Created by rdher on 05/07/2021.
-////
 //
-//#ifndef RAYTRACING_MATRIX_H
-//#define RAYTRACING_MATRIX_H
+// Created by rdher on 05/07/2021.
 //
-//#include <array>
-//#include <vector>
-//
-////Matrix 4x4
-//class Matrix {
-//private:
-//    std::array<float, 4 * 4> m{};
-//
+
+#ifndef RAYTRACING_MATRIX_H
+#define RAYTRACING_MATRIX_H
+
+#include <array>
+#include <vector>
+
+//Matrix 4x4
+class Matrix {
+private:
+    std::array<float, 16> m{};
+
 //    static float invf(int i, int j, const std::array<float, 4 * 4> *mat);
-//
-//    static std::array<float, 4 * 4> emptyMatrix();
-//
-//public:
-//
-//    Matrix();
-//
-//    Matrix(const Matrix &mat);
-//
-//    ~Matrix() = default;
-//
-//    Matrix inverse() const;
-//
-//    float operator()(int i, int j) const;
-//};
-//
-//
-//#endif //RAYTRACING_MATRIX_H
+
+    std::array<float, 16> emptyMatrix();
+
+public:
+
+    Matrix();
+
+    Matrix(const Matrix &mat);
+
+    ~Matrix() = default;
+    float determinant();
+    Matrix adjugate();
+    Matrix inverse() const;
+
+    float operator()(int i, int j)const;
+    float& operator()(int i, int j);
+
+};
+
+
+#endif //RAYTRACING_MATRIX_H
