778475cde63bc91c39a28f9f86342ca6a20162be
Ronan <r.dhersignerie@gmail.com>
2021-07-15 19:13:11 +0200

    Material.h / cpp

---
+++ src/Tools/Material.h
@@ -0,0 +1,50 @@
+//
+// Created by rdher on 15/07/2021.
+//
+
+#ifndef RAYTRACING_MATERIAL_H
+#define RAYTRACING_MATERIAL_H
+
+#include "Color.h"
+
+class Material {
+private:
+    Color ka, kd, ks;
+    float shininess;
+public:
+    void Shininess(const float s) {
+        shininess = s;
+    }
+
+    float Shininess() const {
+        return shininess;
+    }
+
+    void Ka(const Color c) {
+        ka = c;
+    }
+
+    void Kd(const Color c) {
+        kd = c;
+    }
+
+    void Ks(const Color c) {
+        ks = c;
+    }
+
+    Color Ka() const {
+        return ka;
+    }
+
+    Color Kd() const {
+        return kd;
+    }
+
+    Color Ks() const {
+        return ks;
+    }
+
+};
+
+
+#endif //RAYTRACING_MATERIAL_H
